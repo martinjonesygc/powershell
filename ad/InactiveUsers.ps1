a144ab16dac1d40be1c2e590f71f58a8aa7bc14e
martinjonesygc <41617076+martinjonesygc@users.noreply.github.com>
2018-07-25 11:31:37 +0100

    Update InactiveUsers.ps1

--- ad/InactiveUsers.ps1
+++ ad/InactiveUsers.ps1
@@ -1,4 +1,7 @@
-<# Martin Jones 25-06-2018
-Export inactive users to csv change timespan to no of days #>
+<#
+  .Description
+		Exports inactive users to csv, edit -TimeSpan to desired number of days.
+#>
+
 Search-ADAccount -AccountInactive -TimeSpan 90 -UsersOnly | Where-Object { $_.Enabled -eq $true
 } | sort-object name | Format-Table Name, lastlogondate, UserPrincipalName > c:\Inactive-AD-Users.txt
